7f6ae10cb42bf764c0e3445e32da815f4a49bcb3
Matthew Champion <matt.champion@cortex.gg>
2023-07-01 14:38:06 +0100

    Add an additional page for trying out LocationChanging separately

--- Helpers/Helpers.cs
+++ Helpers/Helpers.cs
@@ -4,3 +4,19 @@ public static class StaticStrings
 {
     public const string BlogUrl = "https://mattjameschampion.com/";
 }
+
+public static class LazyLogHelpers
+{
+    public static void LazyLog(this List<IEnumerable<string>> logMessageGroups, params string[] logStrings)
+    {
+        //Just like logging things properly, except for all of the good parts
+        logMessageGroups.Add(logStrings);
+        
+        foreach (var logString in logStrings)
+        {
+            Console.WriteLine(logString);
+        }
+        
+        Console.WriteLine();
+    }
+}
